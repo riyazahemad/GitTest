e506bd29406d0ba284492432ef95843405706942
apple <apple@apples-MacBook-Pro.local>
2014-12-06 14:20:05 +0530

    Impoprting new project

---
+++ HelloGit/HelloGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloGit
+//
+//  Created by apple on 06/12/14.
+//  Copyright (c) 2014 coreData. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloGit/HelloGit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HelloGit
+//
+//  Created by apple on 06/12/14.
+//  Copyright (c) 2014 coreData. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
